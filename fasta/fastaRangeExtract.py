1c7b0aa45baa9a0e43f6ddc7441b6933403ecc2c
Jeff Kimbrel <jakpot@gmail.com>
2018-08-27 13:02:25 -0700

    Extract only from a subsequence

--- fasta/fastaRangeExtract.py
+++ fasta/fastaRangeExtract.py
@@ -1,9 +1,58 @@
 import sys
 from Bio import SeqIO
+import argparse
 
-start = int(sys.argv[2]) - 1
-stop = int(sys.argv[3])
+parser = argparse.ArgumentParser(description='Extract a sequence range from a fasta file')
 
-for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
-    print(">"+seq_record.id)
-    print(seq_record.seq[start:stop])
+parser.add_argument('-f', '--fasta',
+    help="fasta file",
+    required=True)
+
+parser.add_argument('-b', '--begin',
+    help="Beginning nucleotide position (1-based)",
+    type = int,
+    required=True)
+
+parser.add_argument('-s', '--sequence',
+    default = "",
+    help="Only extract from sequence with this header")
+
+parser.add_argument('-e', '--end',
+    help="Ending nucleotide position (1-based)",
+    type = int,
+    required=True)
+
+parser.add_argument('--revcom', '-rc',
+    action = 'store_true',
+    help = 'Reverse Complement?' )
+
+args = parser.parse_args()
+
+args.begin -= 1
+
+##
+
+for seq_record in SeqIO.parse(args.fasta, "fasta"):
+    id = str(seq_record.id)
+
+    if len(args.sequence) > 0:
+
+
+        if id == args.sequence:
+            id = id + "_" + str(args.begin + 1) + "-" + str(args.end)
+
+            if args.revcom == True:
+                print(">" + id + "rc")
+                print(seq_record.seq[args.begin:args.end].reverse_complement())
+            else:
+                print(">" + id)
+                print(seq_record.seq[args.begin:args.end])
+    else:
+        id = id + "_" + str(args.begin + 1) + "-" + str(args.end)
+
+        if args.revcom == True:
+            print(">" + id + "rc")
+            print(seq_record.seq[args.begin:args.end].reverse_complement())
+        else:
+            print(">" + id)
+            print(seq_record.seq[args.begin:args.end])
