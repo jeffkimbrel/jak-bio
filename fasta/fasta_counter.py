a5c463842d6cebf5847d39069d8f0d9b89b8f35c
Jeff Kimbrel <jakpot@gmail.com>
2019-06-18 10:18:12 -0700

    Update fasta_counter.py
    accepts nargs now

--- fasta/fasta_counter.py
+++ fasta/fasta_counter.py
@@ -1,44 +1,55 @@
 from Bio import SeqIO
 import argparse
 
-## OPTIONS #####################################################################
+# OPTIONS #####################################################################
 
-parser = argparse.ArgumentParser(description = 'Given a fasta file, returns the total or subsequence nt/aa count.')
-parser.add_argument('-f', '--fasta', help = "fasta file", required = True)
-parser.add_argument('--total', '-t', action = 'store_true', help = 'Print total of all subsequences' )
-parser.add_argument('--sequence', '-s', action = 'store_true', help = 'Print total for each subsequence' )
+parser = argparse.ArgumentParser(
+    description='Given a fasta file, \
+    returns the total or subsequence nt/aa count.')
+
+parser.add_argument('-f',
+                    '--fasta',
+                    nargs='*',
+                    help="fasta file",
+                    required=True)
+parser.add_argument('--total',
+                    '-t',
+                    action='store_true',
+                    help='Print total of all subsequences')
+parser.add_argument('--sequence',
+                    '-s',
+                    action='store_true',
+                    help='Print total for each subsequence')
+
 args = parser.parse_args()
 
-total = 0
+# FUNCTIONS ###################################################################
 
-## FUNCTIONS ###################################################################
-
-def addToTotal(count):
-    global total
-    total += count
 
 def printSequenceTotal(id, count):
-    if args.sequence == True:
-        print(id, count, sep = "\t")
+    if args.sequence is True:
+        print(id, count, sep="\t")
 
-def printTotal():
-    if args.total == True:
-        print("Total", total, sep = "\t")
 
 def getInfo(seq_record):
     id = str(seq_record.id)
     count = len(str(seq_record.seq))
     return(id, count)
 
-## ANALYZE #####################################################################
+# ANALYZE #####################################################################
 
-for seq_record in SeqIO.parse(args.fasta, "fasta"):
-    id, count = getInfo(seq_record)
-    addToTotal(count)
-    printSequenceTotal(id, count)
 
-printTotal()
+for fasta_file in args.fasta:
+    total = 0
+    for seq_record in SeqIO.parse(fasta_file, "fasta"):
 
-if args.total == False:
-    if args.sequence == False:
+        id, count = getInfo(seq_record)
+        total += count
+        printSequenceTotal(id, count)
+
+    if args.total is True:
+        print(fasta_file, total, sep="\t")
+
+if args.total is False:
+    if args.sequence is False:
         print("\nYou should probably use the -s or -t flags to see some results...\n")
