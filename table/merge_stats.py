e2920560b3c61ffbd37f3750896c873072040acd
Jeff Kimbrel <jakpot@gmail.com>
2019-10-07 10:11:02 -0700

    Create merge_stats.py

---
+++ table/merge_stats.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+
+import argparse
+
+# Arguments
+parser = argparse.ArgumentParser(description='Merge a folder of stats files, such as from bbtools')
+
+parser.add_argument('-d', '--directory', help="Folder containing the files", required=True)
+parser.add_argument('-t', '--text', help="Common file name text to identify files", required=True)
+parser.add_argument('-o', '--out', help="Out file name", required=True)
+
+args = parser.parse_args()
+
+all = None
+
+for fileName in os.listdir(args.directory):
+    if args.text in fileName:
+        split = fileName.split("_")
+        file = pd.read_csv(os.path.join(args.directory, fileName), sep="\t")
+        file['SAMPLE'] = split[0]
+
+        if all is None:
+            all = file
+
+        else:
+            all = pd.concat([all, file])
+
+
+all.to_csv(args.out, sep="\t", index=False)
