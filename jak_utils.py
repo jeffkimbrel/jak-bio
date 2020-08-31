6dab278ec5bdb13a65926d825263aff23c54bfda
Jeff Kimbrel <jakpot@gmail.com>
2020-08-31 13:54:22 -0700

    v0.0.7
    fix so the yaml file can be found from any directory

--- jak_utils.py
+++ jak_utils.py
@@ -1,8 +1,14 @@
 import yaml
+import sys
+import os
 from jakomics import colors
 
 
 def header():
-    with open('jak_meta.yml') as f:
+    code_name = os.path.basename(sys.argv[0])
+    yaml_path = os.path.join(os.path.dirname(sys.argv[0]), 'jak_meta.yml')
+    with open(yaml_path) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-        print(f'{colors.bcolors.BLUE}JAK-BIO v{data["version"]}{colors.bcolors.END}')
+
+    print(
+        f'{colors.bcolors.BLUE}{code_name} v{data["version"]} JAK-BIO{colors.bcolors.END}')
