6b41d6faa642502d6c1d129c34983f98b6118c8d
Jeff Kimbrel <jakpot@gmail.com>
2020-09-03 09:59:42 -0700

    v0.1.4
    combined fasta_counter and fasta_gc to fasta_info

--- fasta_info.py
+++ fasta_info.py
@@ -3,6 +3,7 @@
 from Bio import SeqIO
 import argparse
 import sys
+from Bio.SeqUtils import GC
 
 import jak_utils
 from jakomics import colors, utilities
@@ -55,15 +56,19 @@ fasta_files = utilities.get_files(args.files, args.in_dir, ['faa', 'fa', 'ffn', 
 
 for fasta_file in fasta_files:
     total = 0
+    total_gc = 0
     for seq_record in SeqIO.parse(fasta_file.file_path, "fasta"):
 
         id, count = getInfo(seq_record)
         total += count
+        seq_gc = sum(seq_record.seq.count(x) for x in ["G", "C", "g", "c", "S", "s"])
+        total_gc += seq_gc
+
         if args.sequence is True:
-            print(fasta_file.name, id, count, sep="\t")
+            print(fasta_file.name, id, count, seq_gc / count, sep="\t")
 
     if args.sequence is False:
-        print(fasta_file.name, total, sep="\t")
+        print(fasta_file.name, total, total_gc / total, sep="\t")
 
 if args.total is True and args.sequence is True:
     print(f"{colors.bcolors.YELLOW}WARNING: if both '-s' and '-t' are given, only the sequence lengths will be returned.{colors.bcolors.END}", file=sys.stderr)
