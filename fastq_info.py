f00d1c7a62abc44839bc2fa0b960578e0eca29e6
Jeff Kimbrel <jakpot@gmail.com>
2020-10-12 16:05:10 -0700

    v0.1.11

--- fastq_info.py
+++ fastq_info.py
@@ -2,12 +2,14 @@
 
 import argparse
 import pandas as pd
+from tqdm import tqdm
+from multiprocessing import Pool
 
 from jakomics.fastq import FASTQ, run_info
 from jakomics import colors
 
 import jak_utils
-jak_utils.header()
+
 
 # OPTIONS #####################################################################
 parser = argparse.ArgumentParser(
@@ -23,14 +25,13 @@ parser.add_argument('--md5',
 
 args = parser.parse_args()
 
-files = pd.read_excel(args.samples, index_col=0)
 
-for sample, row in files.iterrows():
-    print(f"Working on {sample}")
-    d = FASTQ(sample, row)
-    for fastq_file in d.files:
+def get_info(sample):
+    for fastq_file in sample.files:
         if args.md5:
             fastq_file.get_md5()
+        else:
+            fastq_file.md5 = "NA"
 
         run_info_results = run_info(fastq_file.file_path)
 
@@ -39,5 +40,21 @@ for sample, row in files.iterrows():
                 f"\n{colors.bcolors.RED}WARNING: {fastq_file.name} appears to be a combination of different Illumina runs!{colors.bcolors.END}")
 
         for result in run_info_results:
-            print(d.sample, fastq_file.name, d.type, fastq_file.read,
-                  result, run_info_results[result], sep="\t")
+            print(sample.sample, fastq_file.name, sample.type, fastq_file.read,
+                  result, run_info_results[result], fastq_file.md5, sep="\t")
+
+
+if __name__ == "__main__":
+    jak_utils.header()
+    files = pd.read_excel(args.samples, index_col=0)
+
+    sample_list = []
+
+    for sample, row in files.iterrows():
+        d = FASTQ(sample, row)
+        sample_list.append(d)
+
+    pool = Pool(processes=8)
+    for _ in tqdm(pool.imap_unordered(get_info, sample_list), total=len(sample_list), desc="Finished", unit=" samples"):
+        pass
+    pool.close()
