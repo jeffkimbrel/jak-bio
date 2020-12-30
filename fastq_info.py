dded6457c5aaef7a39e631fac5f1eee8582c531c
Jeff Kimbrel <jakpot@gmail.com>
2020-12-30 08:37:42 -0800

    v0.6.2
    fastq_filter:
    output to file
    each input file result is on a single line

--- fastq_info.py
+++ fastq_info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import pandas as pd
 from tqdm import tqdm
 from multiprocessing import Pool, Manager
@@ -17,12 +18,16 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-s', '--samples',
                     help="excel file with samples in S, F, R, I columns",
-                    required=False)
+                    required=True)
 
 parser.add_argument('--md5',
                     action='store_true',
                     help='Run md5 (very slow)')
 
+parser.add_argument('--out',
+                    help="File to write results to",
+                    default="fastq_info_results.txt",
+                    required=False)
 
 args = parser.parse_args()
 
@@ -39,18 +44,25 @@ def get_info(sample):
 
         run_info_results = run_info(fastq_file.file_path)
 
-        if len(run_info_results) > 1:
-            print(
-                f"\n{colors.bcolors.RED}WARNING: {fastq_file.name} appears to be a combination of different Illumina runs!{colors.bcolors.END}")
+        file_results = {"SAMPLE": sample.sample,
+                        "FILE": fastq_file.file_path,
+                        "MD5": fastq_file.md5,
+                        "TYPE": sample.type,
+                        "PAIR": fastq_file.read,
+                        "TOTAL_READS": 0,
+                        "RUN_INFO": {},
+                        }
 
         for result in run_info_results:
-            results.append([sample.sample, fastq_file.name, sample.type, fastq_file.read,
-                            result, run_info_results[result], fastq_file.md5])
+            file_results["RUN_INFO"][result] = run_info_results[result]
+            file_results["TOTAL_READS"] += run_info_results[result]
+
+        results[sample.sample + "_" + fastq_file.read] = file_results
 
 
 if __name__ == "__main__":
     manager = Manager()
-    results = manager.list()
+    results = manager.dict()
 
     jak_utils.header()
     files = pd.read_excel(args.samples, index_col=0, engine='openpyxl')
@@ -66,5 +78,32 @@ if __name__ == "__main__":
         pass
     pool.close()
 
+    df = pd.DataFrame(columns=['INDEX', 'SAMPLE', 'FILE', 'MD5',
+                               'TYPE', 'PAIR', 'TOTAL_READS', 'RUN_INFO'])
+
     for result in results:
-        print(result)
+        df = df.append(
+            pd.Series(data={
+                'INDEX': result,
+                'SAMPLE': results[result]["SAMPLE"],
+                'FILE': results[result]["FILE"],
+                'MD5': results[result]["MD5"],
+                'TYPE': results[result]["TYPE"],
+                'PAIR': results[result]["PAIR"],
+                'TOTAL_READS': results[result]["TOTAL_READS"],
+                'RUN_INFO': str(results[result]["RUN_INFO"]),
+
+            }
+            ),
+            ignore_index=True)
+
+    # write to file with comments
+    if os.path.exists(args.out):
+        os.remove(args.out)
+    f = open(args.out, 'a')
+    for c in jak_utils.header(r=True):
+        print(f'# {c}', file=f)
+    for arg in vars(args):
+        print(f'# ARG {arg} = {getattr(args, arg)}', file=f)
+
+    df.to_csv(f, sep="\t", index=False)
