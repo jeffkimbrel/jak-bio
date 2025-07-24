874eb533b9a40d784e4ba1177106bcc0035b6f83
Jeff Kimbrel <jakpot@gmail.com>
2025-07-24 10:33:07 -0700

    importlib instead of pkg_resources

--- jak_utils.py
+++ jak_utils.py
@@ -1,18 +1,18 @@
 import yaml
 import sys
 import os
-import pkg_resources
+import importlib.metadata
 import datetime
 from jakomics import colors
 
 
-def header(r=False):
+def header(r = False):
     code_name = os.path.basename(sys.argv[0])
     yaml_path = os.path.join(os.path.dirname(sys.argv[0]), 'jak_meta.yml')
     with open(yaml_path) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
 
-    j_version = pkg_resources.get_distribution("jakomics").version
+    j_version = importlib.metadata.version("jakomics")
 
     if r == False:
         print(
